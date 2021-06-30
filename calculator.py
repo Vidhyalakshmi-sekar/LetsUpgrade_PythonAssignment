ce09945e4a7dedc5397d084d2f124b86ed1d6a2c
Vidhyalakshmi-sekar <86703843+Vidhyalakshmi-sekar@users.noreply.github.com>
2021-06-30 10:40:29 +0530

    Day 1 Assignment

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# Getting inputs from the users
+
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
+
+add = first_number + second_number
+sub = first_number - second_number
+mul = first_number * second_number
+float_div = first_number / second_number
+int_div = first_number // second_number
+
+# float_div = Floating point Division
+# int_div = Integer Division
+
+print("Our Addition is", add)
+print("Our Subtraction is", sub)
+print("Our Multiplication is", mul)
+print("Our Float point division is", float_div)
+print("Our Integer division is", int_div)
